75cb6b5b07e507a2be28f536e75444447ec865f8
zeinabnimer <www.zeinab6@gmail.com>
2018-06-25 15:15:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,2 @@
 class Song
-  ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
-
-  ninety_nine_problems.name
-
-  ninety_nine_problems.artist
-
-ninety_nine_problems.genre  
-end
-
+  
